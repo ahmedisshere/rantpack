8da26421828bdcce21c67724fc97adda81e86148
Tausif Ahmed <tausifahmed471@gmail.com>
2022-02-20 07:41:27 +0600

    how to make a class component dynamic

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { Component } from "react";
 
-let Person = (props) => {
-    console.log(props);
-    return (
-        <div>
-            <h3> Gamer {props.gamer} & Rank {props.rank} </h3>
-            <h4>Intel: {props.children} </h4>
-        </div>
-    );
+// let Person = (props) => {
+//     console.log(props);
+//     return (
+//         <div>
+//             <h3> Gamer {props.gamer} & Rank {props.rank} </h3>
+//             <h4>Intel: {props.children} </h4>
+//         </div>
+//     );
+// }
+
+class Person extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                <h3> Gamer {this.props.gamer} & Rank {this.props.rank} </h3>
+                <h4>Intel: {this.props.children} </h4>
+            </div>
+        );
+    }
 }
+
 
 export default Person;
