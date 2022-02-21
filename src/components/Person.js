dbced327aafb1cde108d631f36899f2d38dd9b00
Tausif Ahmed <tausifahmed471@gmail.com>
2022-02-21 08:27:03 +0600

    new components

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,14 +1,5 @@
 import React, { Component } from "react";
 
-// let Person = (props) => {
-//     console.log(props);
-//     return (
-//         <div>
-//             <h3> Gamer {props.gamer} & Rank {props.rank} </h3>
-//             <h4>Intel: {props.children} </h4>
-//         </div>
-//     );
-// }
 
 class Person extends Component {
     constructor(props) {
