84db0472cd5bbf508d45edbbc4547483d28ec034
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-07 05:09:00 +0600

    minor styles & script

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,9 +1,10 @@
 import React from "react";
+import '../stylesheets/Book.css';
 
 const Book = props => {
 
     return (
-        <div>
+        <div className="Book">
             <h3 onClick={props.change}>Book: {props.bookName} </h3>
             <h4>Writter: {props.writer}</h4>
             <input type="text" onChange={props.inputName} value={props.bookName}/>
