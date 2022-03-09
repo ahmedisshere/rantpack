7aaa65e0b292cdeca750c61b991583fc816e5da3
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-10 05:36:36 +0600

    toggling states

--- src/App.js
+++ src/App.js
@@ -14,7 +14,14 @@ class App extends Component {
       { id:2 ,bookName: "Da Vinci Code", writer: "Dan Brown" },
       { id:3 ,bookName: "The Alchemist", writer: "Paulo CoelhO" }
     ],
+    showBooks: true
   };
+
+
+
+
+
+
 
   changeWithInputState = (event,index) => {
 
@@ -26,21 +33,44 @@ class App extends Component {
     const books = [...this.state.books];
     books[index] = book;
 
-    this.setState({books: books });
+    this.setState({books: books});
   }
 
+
+
+
+
+
+
   deleteBookState = index => {
+
     // const  books = this.state.books.slice();
     // const books = this.state.books.map(item => item);
 
     const books = [...this.state.books];
     books.splice(index, 1);
     this.setState({
-
       books: books
-
     });
   };
+
+
+
+
+
+
+
+
+  toggleBooks = () => { 
+    this.setState({showBooks: !this.state.showBooks});
+  }
+
+
+
+
+
+
+
 
   render() {
 
@@ -50,6 +80,11 @@ class App extends Component {
       backgroundColor: "rgb(9, 39, 22)",
       color: "white"
     };
+
+
+
+
+
 
 
     const books = this.state.books.map((book,index) => {
@@ -65,18 +100,27 @@ class App extends Component {
       
     });
 
+
+
+
+
+
+
     return (
 
       <div className="App">
-        <h1 style={style}> Book List </h1>
+        <h1 style={style}> Random States </h1>
+        <button onClick={this.toggleBooks}>Toggle Books</button>
 
-        {books}
+       {this.state.showBooks ? books : null }
 
       </div>
 
     );
   }
 }
+
+
 // State -> state a components own information,state is generally declared on class component
 
 // -->> we can define state in outside of the constructor -> in a class function
