3b748df912ca0500ad89f26a6757e52c0750e03d
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-12 04:32:26 +0600

    lets get back

--- src/App.js
+++ src/App.js
@@ -14,6 +14,6 @@ const App = () => {
 // 16.8 includes react hooks which is somekind of similar feature
 // State -> state a components own information,state is generally declared on class component
 
-// -->> we can define state in outside of the constructor -> in a class function
+// -->> we can define state in the outside of the constructor -> in a class function
 
 export default App;
