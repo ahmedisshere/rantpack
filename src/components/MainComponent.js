8dd0a066c824d1ef9e9d62b923ce6e957c0bd1bb
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-18 07:07:41 +0600

    focussss

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -62,7 +62,7 @@ class MainComponent extends Component {
     render() {
 
         const style = {
-            border: "2px solid red",
+            border: "2px solid tomato",
             borderRadius: "5px",
             backgroundColor: "rgb(9, 39, 22)",
             color: "white",
